1b70901d167739a49a12091457db90c5c2e21944
rajk <rajkarekar@edbrix.com>
2018-02-08 10:38:49 +0530

    -Video List Adapter committed

---
+++ app/src/main/java/com/edbrix/enterprise/Adapters/VideoListAdapter.java
@@ -0,0 +1,156 @@
+package com.edbrix.enterprise.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.edbrix.enterprise.Activities.VideoPlayerActivity;
+import com.edbrix.enterprise.Models.FileData;
+import com.edbrix.enterprise.R;
+import com.edbrix.enterprise.commons.VideoThumbLoader;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by rajk on 30/01/18.
+ */
+
+public class VideoListAdapter extends RecyclerView.Adapter<VideoListAdapter.VideoViewHolder> {
+    private Context adContext;
+    private List<FileData> fileDataList;
+    private OnButtonActionListener onButtonActionListener;
+    private final VideoThumbLoader mVideoThumbLoader;
+
+    public interface OnButtonActionListener {
+
+        public void onDeleteButtonPressed(FileData fileData, int position);
+
+        public void onCardViewClicked(FileData fileData, int position);
+
+    }
+
+    public VideoListAdapter(Context adContext, List<FileData> filesList, OnButtonActionListener onButtonActionListener) {
+        this.fileDataList = filesList;
+        this.adContext = adContext;
+        this.mVideoThumbLoader = new VideoThumbLoader();
+        this.onButtonActionListener = onButtonActionListener;
+    }
+
+    @Override
+    public VideoListAdapter.VideoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_video_list, parent, false);
+
+        return new VideoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final VideoListAdapter.VideoViewHolder holder, final int position) {
+        holder.fileDataAdpt = fileDataList.get(position);
+        holder.thumbnail.setTag(holder.fileDataAdpt.getFileObject().getPath());// binding imageview
+
+        holder.thumbnail.setImageResource(R.drawable.play_circle_grey); //default image
+        mVideoThumbLoader.showThumbByAsynctack(holder.fileDataAdpt.getFileObject().getPath(), holder.thumbnail);
+        String fName = holder.fileDataAdpt.getFileName().replaceAll(".mp4", "");
+        holder.title.setText(fName);
+        holder.duration.setText(getVideoDuration(adContext, holder.fileDataAdpt.getFileObject()));
+        holder.thumbnail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openVideoPlayer(holder.fileDataAdpt);
+            }
+        });
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onButtonActionListener.onCardViewClicked(holder.fileDataAdpt,position);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return fileDataList.size();
+    }
+
+    public class VideoViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, duration;
+        public ImageView thumbnail;
+        public FileData fileDataAdpt;
+        public View mView;
+
+        public VideoViewHolder(View view) {
+            super(view);
+            mView =view;
+            title = (TextView) view.findViewById(R.id.videoName);
+            duration = (TextView) view.findViewById(R.id.videoDuration);
+            thumbnail = (ImageView) view.findViewById(R.id.videoThumbnail);
+
+        }
+    }
+
+    /**
+     * Return video file duration from File Object
+     *
+     * @param context   Context
+     * @param uriOfFile URI of File
+     * @return String with min:sec format
+     */
+    public String getVideoDuration(Context context, File uriOfFile) {
+        int duration = 0;
+        MediaPlayer mp = MediaPlayer.create(context, Uri.parse(uriOfFile.getPath()));
+        try {
+            duration = mp.getDuration();
+            mp.release();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+/*convert millis to appropriate time*/
+        return String.format("%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(duration),
+                TimeUnit.MILLISECONDS.toSeconds(duration) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)));
+    }
+
+    public void removeItem(int pos) {
+        if (fileDataList != null && fileDataList.size() > 0) {
+            fileDataList.remove(pos);
+            notifyDataSetChanged();
+        }
+    }
+
+    private void openVideoPlayer(FileData fileData) {
+        Intent videoPlayer = new Intent(adContext, VideoPlayerActivity.class);
+        videoPlayer.putExtra("FileData", fileData);
+        adContext.startActivity(videoPlayer);
+    }
+
+    public void updateList(List<FileData> list) {
+        if (fileDataList != null && fileDataList.size() > 0) {
+            fileDataList = list;
+            notifyDataSetChanged();
+        }
+    }
+
+    public void refresh(ArrayList<FileData> list) {
+        this.fileDataList = new ArrayList<>();
+        if (list != null && !list.isEmpty()) {
+            this.fileDataList = list;
+        }
+        notifyDataSetChanged();
+
+    }
+
+}
